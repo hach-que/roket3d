234260d6c1cf4246a3dcccf5abe5eae3022f96eb
hachque <hachque@localhost>
2010-08-25 21:59:17 +1000

    Removed files left over from moving.

--- Engine/Core/Headers/E.Unmanaged/Physics/roket_physicscallbacks.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_ROKETPHYSICSCALLBACKS
-#define HEADER_ROKETPHYSICSCALLBACKS
-namespace physics
-{
-	void _cdecl TransformCallback(const NewtonBody* body, const dFloat* matrix, int threadIndex);
-	void _cdecl ApplyForceAndTorqueCallback(const NewtonBody* body, dFloat timestep, int threadIndex);
-	void _cdecl JointDestructor(const NewtonJoint* joint);
-}
-#endif
