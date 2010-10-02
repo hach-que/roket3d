566bdb1603a89bf7b1e68c3aa2b5ee865ee2d49b
hachque <hachque@localhost>
2010-10-02 18:00:19 +1000

    Added all of the new GUI elements files (basic class structure implemented, no functionality). Added support for creating checkboxes, color select dialogs, comboboxes, context menus and editboxes to RGUIEnvironment. Added implicit string conversion class (removed ToUnicode and ToASCII functions).

--- Engine/Testing/Game.rs
+++ Engine/Testing/Game.rs
@@ -11,6 +11,32 @@ function Run()
 		Engine.Collections.Rectangle(20, 20, 100, 40),
 		nil, nil, "My Button!")
     
+    -- Add a GUI checkbox
+    checkbox = device.GUIEnvironment:AddCheckBox(
+		false,
+		Engine.Collections.Rectangle(20, 50, 100, 70),
+		nil, nil, "My Checkbox")
+    
+    -- Add a GUI color select dialog
+    dialog = device.GUIEnvironment:AddColorSelectDialog(
+		"Select Color", false)
+		
+    -- Add a GUI combobox
+    combobox = device.GUIEnvironment:AddComboBox(
+		Engine.Collections.Rectangle(20, 80, 100, 100),
+		nil, nil)
+    
+    -- Add a GUI context menu
+    context = device.GUIEnvironment:AddContextMenu(
+		Engine.Collections.Rectangle(140, 20, 220, 100),
+		nil, nil)
+    
+    -- Add a GUI editbox
+    editbox = device.GUIEnvironment:AddEditBox(
+		"My Content",
+		Engine.Collections.Rectangle(20, 110, 100, 130),
+		true, nil, nil)
+    
     while (device.Running) do
         if (up) then
             backcolor.Green = backcolor.Green + 1
--- Utility/AutoBind/Include/autobind/types.h
+++ Utility/AutoBind/Include/autobind/types.h
@@ -10,11 +10,79 @@
 
 // Include Lua.
 #include <lua.hpp>
+#include <malloc.h>
+#include <string.h>
+#include <string>
 
 typedef lua_Number numeric;
-typedef const char* string;
-typedef const wchar_t* wstring;
 typedef void table;
+
+class string
+{
+public:
+	inline string(const char* text)
+	{
+		// Allocate memory to be used by the string.
+		this->len = strlen(text);
+		this->data = (wchar_t*)malloc((this->len + 1) * sizeof(wchar_t));
+		for (size_t i = 0; i < this->len; i += 1)
+			this->data[i] = text[i];
+		this->data[this->len] = 0; // NULL terminator.
+	}
+
+	inline string(const wchar_t* text)
+	{
+		// Allocate memory to be used by the string.
+		this->len = wcslen(text);
+		this->data = (wchar_t*)malloc((this->len + 1) * sizeof(wchar_t));
+		for (size_t i = 0; i < this->len; i += 1)
+			this->data[i] = text[i];
+		this->data[this->len] = 0; // NULL terminator.
+	}
+
+	inline ~string()
+	{
+		// Free the memory used by the string.
+		free(this->data);
+	}
+
+	operator const char*()
+	{
+		// Implicit conversion to const char*.  We have to
+		// malloc a new string and return it as a reference
+		// so that it's still valid when this object goes out
+		// of scope.
+		char* ret = (char*)malloc((len + 1) * sizeof(char));
+		for (size_t i = 0; i < len; i += 1)
+			ret[i] = this->data[i];
+		ret[len] = 0; // NULL terminator.
+		return const_cast<const char*>(ret);
+	}
+
+	operator const wchar_t*()
+	{
+		// Implicit conversion to const wchar_t*.  We have to
+		// malloc a new string and return it as a reference
+		// so that it's still valid when this object goes out
+		// of scope.
+		wchar_t* ret = (wchar_t*)malloc((len + 1) * sizeof(wchar_t));
+		for (size_t i = 0; i < len; i += 1)
+			ret[i] = this->data[i];
+		ret[len] = 0; // NULL terminator.
+		return const_cast<const wchar_t*>(ret);
+	}
+
+	operator const std::string()
+	{
+		// Implicit conversion to const std::string.
+		const std::string ret = (const char*)this->data;
+		return ret;
+	}
+
+private:
+	wchar_t* data;
+	size_t len;
+};
 
 // TODO: Replace the typedefs with actual classes
 //       which properly handle the conversion and
