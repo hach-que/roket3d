b55a69286b2db7f57fda400566ba2130d1f1b82d
hachque <hachque@localhost>
2010-10-03 13:42:55 +1100

    Added basic table class to types.h.  Get Rob to implement functionality of it (except PushAsResult).

--- Utility/AutoBind/Include/autobind/types.h
+++ Utility/AutoBind/Include/autobind/types.h
@@ -13,9 +13,9 @@
 #include <malloc.h>
 #include <string.h>
 #include <string>
+#include <map>
 
 typedef lua_Number numeric;
-typedef void table;
 
 class string
 {
@@ -83,6 +83,303 @@ private:
 	wchar_t* data;
 	size_t len;
 };
+
+template<class T>
+	class table
+	{
+	public:
+		// Enum for returning the type of object.
+		typedef enum
+		{
+			t_nil,
+			t_bool,
+			t_numeric,
+			t_string,
+			t_table,
+			t_object,
+			t_other
+		} ValueType;
+
+	private:
+		// Structure for storing keys and values within m_Data.
+		typedef struct
+		{
+			m_TypeStorage type;
+			union
+			{
+				bool b;
+				numeric n;
+				string s;
+				table * t;
+				T * o;
+			} store;
+
+			struct(bool v)
+			{
+				type = t_bool;
+				store.b = v;
+			}
+
+			struct(numeric v)
+			{
+				type = t_numeric;
+				store.n = v;
+			}
+
+			struct(string v)
+			{
+				type = t_string;
+				store.s = v;
+			}
+
+			struct(table * v)
+			{
+				type = t_table;
+				store.t = v;
+			}
+
+			struct(T * v)
+			{
+				type = t_object;
+				store.o = v;
+			}
+		} m_ValueStorage;
+
+	public:
+		// Structure that's returned when accessing via index.  Overloads
+		// the assignment operator so that C++ users can assign to the
+		// variable.
+		typedef struct
+		{
+		private:
+			table * container;
+			m_ValueStorage key;
+			m_ValueStorage value;
+
+		public:
+			ValueType type;
+
+			struct(m_ValueStorage key, m_ValueStorage value)
+			{
+				type = value.type;
+			}
+
+			value& operator =(const bool& v)
+			{
+				container->Set(key, v);
+				value.store.b = v;
+				type = t_bool;
+			}
+
+			value& operator =(const numeric& v)
+			{
+				container->Set(key, v);
+				value.store.n = v;
+				type = t_numeric;
+			}
+
+			value& operator =(const string& v)
+			{
+				container->Set(key, v);
+				value.store.s = v;
+				type = t_string;
+			}
+
+			value& operator =(const table& v)
+			{
+				container->Set(key, v);
+				value.store.t = v;
+				type = t_table;
+			}
+
+			value& operator =(const T*& v)
+			{
+				container->Set(key, v);
+				value.store.o = v;
+				type = t_object;
+			}
+
+			operator bool()
+			{
+				if (value.type == t_bool)
+					return value.b;
+			}
+
+			operator numeric()
+			{
+				if (value.type == t_numeric)
+					return value.n;
+			}
+
+			operator string()
+			{
+				if (value.type == t_string)
+					return value.s;
+			}
+
+			operator table *()
+			{
+				if (value.type == t_table)
+					return value.t;
+			}
+
+			operator T *()
+			{
+				if (value.type == t_object)
+					return value.o;
+			}
+		} value;
+
+		inline table()
+		{
+		}
+
+		inline table(table & copy)
+		{
+			// Copy contents of the other tables m_Data.
+		}
+
+		value operator [](bool key)
+		{
+			return value(m_ValueStorage(key), this->GetValueByKey(m_ValueStorage(key)));
+		}
+
+		value operator [](numeric key)
+		{
+			return value(m_ValueStorage(key), this->GetValueByKey(m_ValueStorage(key)));
+		}
+
+		value operator [](string key)
+		{
+			return value(m_ValueStorage(key), this->GetValueByKey(m_ValueStorage(key)));
+		}
+
+		value operator [](table key)
+		{
+			return value(m_ValueStorage(key), this->GetValueByKey(m_ValueStorage(key)));
+		}
+
+		value operator [](T * key)
+		{
+			return value(m_ValueStorage(key), this->GetValueByKey(m_ValueStorage(key)));
+		}
+
+		inline void Set(bool key, bool value)
+		{
+		}
+
+		inline void Set(numeric key, bool value)
+		{
+		}
+
+		inline void Set(string key, bool value)
+		{
+		}
+
+		inline void Set(table key, bool value)
+		{
+		}
+
+		inline void Set(T * key, bool value)
+		{
+		}
+
+		inline void Set(bool key, numeric value)
+		{
+		}
+
+		inline void Set(numeric key, numeric value)
+		{
+		}
+
+		inline void Set(string key, numeric value)
+		{
+		}
+
+		inline void Set(table key, numeric value)
+		{
+		}
+
+		inline void Set(T * key, numeric value)
+		{
+		}
+
+		inline void Set(bool key, string value)
+		{
+		}
+
+		inline void Set(numeric key, string value)
+		{
+		}
+
+		inline void Set(string key, string value)
+		{
+		}
+
+		inline void Set(table key, string value)
+		{
+		}
+
+		inline void Set(T * key, string value)
+		{
+		}
+
+		inline void Set(bool key, table value)
+		{
+		}
+
+		inline void Set(numeric key, table value)
+		{
+		}
+
+		inline void Set(string key, table value)
+		{
+		}
+
+		inline void Set(table key, table value)
+		{
+		}
+
+		inline void Set(T * key, table value)
+		{
+		}
+
+		inline void Set(bool key, T * value)
+		{
+		}
+
+		inline void Set(numeric key, T * value)
+		{
+		}
+
+		inline void Set(string key, T * value)
+		{
+		}
+
+		inline void Set(table key, T * value)
+		{
+		}
+
+		inline void Set(T * key, T * value)
+		{
+		}
+
+		inline int GetTableIndex()
+		{
+			return this->m_TableIndex;
+		}
+
+		inline void PushAsResult()
+		{
+		}
+
+	private:
+		m_ValueStorage GetValueByKey(m_ValueStorage key)
+		{
+		}
+
+		std::map<m_ValueStorage, m_ValueStorage> m_Data;
+	}
 
 // TODO: Replace the typedefs with actual classes
 //       which properly handle the conversion and
